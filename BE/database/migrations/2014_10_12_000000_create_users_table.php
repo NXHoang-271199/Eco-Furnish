24b2abb58142db3adbd38f7473e305270e032971
NXHoang-271199 <nguyenxuanhoang271199@gmail.com>
2025-02-13 10:22:17 +0700

    create migration

--- BE/database/migrations/2014_10_12_000000_create_users_table.php
+++ BE/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->integer('age');
+            $table->string('email', 255)->unique(); // Email người dùng, đảm bảo không trùng
+            $table->string('password', 255); // Mật khẩu người dùng
+            $table->string('address', 255)->nullable(); // Địa chỉ người dùng, có thể null
+            $table->unsignedBigInteger('role_id'); // ID phân quyền người dùng
+            $table->text('avatar')->nullable(); // Ảnh người dùng, có thể null
+            $table->timestamps(); // Tạo 2 cột created_at & updated_at tự động
         });
     }
 
