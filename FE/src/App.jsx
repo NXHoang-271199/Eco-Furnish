31af158bbc387dad5c47fe4fdba07407aed79727
unknown <datdt2k3>
2025-02-16 04:10:51 +0700

    trang chủ cần them hieu ung

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,15 +1,28 @@
 import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/(website)/home/HomePage";
+import { Route, Routes } from "react-router-dom";
 import LayoutWebsite from "./pages/layout";
-
+import Homes from "./pages/Home/Homes";
+import Products from "./pages/Products/Products";
+import ProductDetail from "./pages/Detail/ProductDetail";
+import Contact from "./pages/Contact/Contact";
+import Cart from "./pages/Cart/Cart";
+import Blogs from "./pages/Blogs/Blogs";
+import About from "./pages/About/About";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LayoutWebsite />}>
-          <Route index element={<HomePage />}></Route>
+          <Route index element={<Homes />} />
+          <Route path="products" element={<Products />} />
+          <Route path="product" element={<ProductDetail />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="about" element={<About />} />
         </Route>
       </Routes>
     </>
---
+++ FE/src/pages/Products/Products.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <>
+      {/* banner */}
+      <section></section>
+    </>
+  );
+};
+
+export default Products;
