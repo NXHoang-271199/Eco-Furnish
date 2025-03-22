98a8f13ad41719184aca83efe730523d3a9f0393
NXHoang-271199 <nguyenxuanhoang271199@gmail.com>
2025-03-22 15:35:23 +0700

    update chat realtime cần login

--- BE/resources/views/layouts/admin.blade.php
+++ BE/resources/views/layouts/admin.blade.php
@@ -11,6 +11,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title')</title>
     {{-- Điền các link CSS dùng chung --}}
