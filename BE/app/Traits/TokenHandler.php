5004d2a729011ca615b13291340244b7afdba81a
datdt2k3 <dinhtiendat11112003@gmail.com>
2025-04-05 09:43:32 +0700

    commit trước khi pull code việt

--- BE/app/Traits/TokenHandler.php
+++ BE/app/Traits/TokenHandler.php
@@ -35,8 +35,8 @@ trait TokenHandler
         return [
             'access_token' => $accessToken,
             'refresh_token' => $refreshToken,
-            'access_token_expires_at' => $accessTokenExpiry,
-            'refresh_token_expires_at' => $refreshTokenExpiry
+            // 'access_token_expires_at' => $accessTokenExpiry,
+            // 'refresh_token_expires_at' => $refreshTokenExpiry
         ];
     }
 
