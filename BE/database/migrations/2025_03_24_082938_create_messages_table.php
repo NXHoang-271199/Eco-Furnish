f6f283c7b0d62492af0f59898c4965bc338632fc
ducvietaz420 <ducvietaz420@gmail.com>
2025-04-08 00:20:28 +0700

    Chỉnh sửa bảng 'messages' để trường 'text', 'image' và 'images' đều có thể null. Xóa nhiều tệp hình ảnh không còn sử dụng trong thư mục uploads. Cập nhật cấu trúc mã trong server.js để đảm bảo không có dòng trống ở cuối tệp.

--- BE/database/migrations/2025_03_24_082938_create_messages_table.php
+++ BE/database/migrations/2025_03_24_082938_create_messages_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->text('text');
-            $table->string('image')->nullable();
-            $table->string('images')->nullable();
+            $table->text('text')->nullable();
+            $table->text('image')->nullable();
+            $table->text('images')->nullable();
             // Người gửi (bắt buộc phải có)
             $table->unsignedBigInteger('sender_id');
             $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
