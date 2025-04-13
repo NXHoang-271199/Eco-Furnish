94ce1e9607f346a909957bfe41279ea50930f83c
NXHoang-271199 <nguyenxuanhoang271199@gmail.com>
2025-04-13 11:31:48 +0700

    update library

--- BE/resources/views/layouts/auth.blade.php
+++ BE/resources/views/layouts/auth.blade.php
@@ -45,6 +45,11 @@
     <script src="{{ asset('assets/admins/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
     <script src="{{ asset('assets/admins/js/plugins.js') }}"></script>
 
+    <!-- particles js -->
+    <script src="{{asset('admins/libs/particles.js/particles.js')}}"></script>
+    <!-- particles app js -->
+    <script src="{{asset('admins/js/pages/particles.app.js')}}"></script>
+    <!-- validation init -->
     <!-- password-addon init -->
     <script src="{{asset('assets/admins/js/pages/password-addon.init.js') }}"></script>
 </body>
