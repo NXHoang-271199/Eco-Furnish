727ab0ddfdbb5a3da7bb4c60b4f4a8554b758316
NXHoang-271199 <nguyenxuanhoang271199@gmail.com>
2025-04-13 11:10:16 +0700

    update layouts/auth

--- BE/resources/views/layouts/auth.blade.php
+++ BE/resources/views/layouts/auth.blade.php
@@ -45,7 +45,8 @@
     <script src="{{ asset('assets/admins/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
     <script src="{{ asset('assets/admins/js/plugins.js') }}"></script>
 
-
+    <!-- particles app js -->
+    <script src="{{asset('admins/js/pages/particles.app.js')}}"></script>
     <!-- password-addon init -->
     <script src="{{asset('assets/admins/js/pages/password-addon.init.js') }}"></script>
 </body>
